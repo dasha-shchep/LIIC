4920dc4855e2655bb6f9d42381271a16ad16c234
dasha-shchep <ds17094@bristol.ac.uk>
2019-11-26 15:36:32 +0000

    Created input flags

--- liic.jl
+++ liic.jl
@@ -1,4 +1,70 @@
 # Julia LIIC Script
 # Written on 26 November 2019
+# Execute by running `julia liic.jl`
 
 
+using DelimitedFiles
+using LinearAlgebra
+using ArgParse
+
+parse_settings = ArgParseSettings()
+
+@add_arg_table parse_settings begin
+    "--cartesian", "-c"
+        help = "Specifies cartesian coordinate interpolation"
+        action = :store_true
+    "--distance", "-d"
+        help = "Specifies distance coordinate interpolation"
+        action = :store_true
+    "--internal", "-i"
+        help = "Specifies interatomic distance interpolation"
+        action = :store_true
+    "geom1"
+        help = "path to .xyz file containing first geometry"
+        required = true
+    "geom2"
+        help = "path to .xyz file containing first geometry"
+        required = true
+end
+
+function xyz2matrix(input_xyz)
+    # Imports .xyz file format as data frame, and converts it to an N x 3 Matrix
+    raw_xyz=readdlm(input_xyz)
+    natoms=size(raw_xyz)[1]
+    just_coords=Array{Float64}(raw_xyz[2:natoms,2:4])
+    return just_coords
+end
+
+function cartesian_interpolation()
+    println("execute cii")
+end
+
+function distance_interpolation()
+    println("execute dii")
+end
+
+function internal_interpolation()
+    println("execute iii")
+end
+
+function main()
+    parsed_args = parse_args(ARGS, parse_settings)
+
+    if parsed_args["cartesian"]
+        println("cart")
+    elseif parsed_args["distance"]
+        println("distance")
+    elseif parsed_args["internal"]
+        println("internal")
+    else
+        println("what")
+    end
+
+    # println(parsed_args["cartesian"])
+    # println("Parsed args:")
+    # for (arg,val) in parsed_args
+    #     println("  $arg  =>  $val")
+    # end
+end
+
+main()
