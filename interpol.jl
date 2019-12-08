280c6cee84b79e83fdabc5991822047d49531c7f
dasha-shchep <ds17094@bristol.ac.uk>
2019-12-08 11:03:22 +0000

    Changed geom to Float32

--- interpol.jl
+++ interpol.jl
@@ -3,17 +3,13 @@ module Interpol
 
 using DelimitedFiles
 
-export cartesian
-export distance
-export internal
-export xyz2matrix
-export writeFile
+export cartesian, distance, internal, xyz2matrix, writeFile
 
 function cartesian(inMat1,inMat2,steps)
     # For an N atom system, this function returns an N x 3 x nsteps array
     difMat = (inMat2 - inMat1) / (steps - 1)
     natoms = size(difMat)[1]
-    interpollatedArray = Array{Float64,3}(undef,natoms,3,steps)
+    interpollatedArray = Array{Float32,3}(undef,natoms,3,steps)
     for i in 0:(steps - 1)
         interpollatedArray[:,:,i+1] = inMat1 + i * difMat
     end
@@ -45,7 +41,7 @@ function xyz2matrix(input_xyz)
     # Imports .xyz file format as data frame, converts it to an N x 3 Matrix
     raw_xyz =   readdlm(input_xyz)
     natoms  =   size(raw_xyz)[1]
-    just_coords = Array{Float64}(raw_xyz[2:end,2:4])
+    just_coords = Array{Float32}(raw_xyz[2:end,2:4])
     return just_coords
 end
 
