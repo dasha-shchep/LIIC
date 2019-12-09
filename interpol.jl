d3e78f3e02956f4e29db1d32630653631671b95c
dasha-shchep <ds17094@bristol.ac.uk>
2019-12-09 14:31:22 +0000

    Commented the functions

--- interpol.jl
+++ interpol.jl
@@ -2,11 +2,12 @@
 module Interpol
 
 using DelimitedFiles
-
 export cartesian, distance, internal, xyz2matrix, writeFile
 
 function cartesian(inMat1,inMat2,steps)
-    # For an N atom system, this function returns an N x 3 x nsteps array
+	"""
+    For an N atom system, this function returns a 3D, N x 3 x nsteps, array
+	"""
     diffMat = (inMat2 - inMat1) / (steps - 1)
     natoms = size(diffMat)[1]
     interpollatedArray = Array{Float32,3}(undef,natoms,3,steps)
@@ -39,6 +40,11 @@ function internal(int_arr_1,int_arr_2,steps,header)
 end
 
 function intlVec(int_arr_1,diffVec,step)
+	"""
+	Function generates set of internal coordinates as a string at a particular
+	step during the interpollation to concatenate with a header string to output
+	a Z-matrix format string.
+	"""
 	intlCrd = Vector(undef,length(diffVec))
 	for i in 1:length(diffVec)
 		intlCrd[i] = string(int_arr_1[i,1]," ",(int_arr_1[i,2]+step*diffVec[i]),"\n")
@@ -60,7 +66,9 @@ function writeFile(output_array,atom_names,file)
 end
 
 function xyz2matrix(input_xyz)
-    # Imports .xyz file format as data frame, converts it to an N x 3 Matrix
+	"""
+    Imports .xyz file format as data frame, converts it to an N x 3 Matrix
+	"""
     raw_xyz =   readdlm(input_xyz)
     just_coords = Array{Float32}(raw_xyz[2:end,2:4])
     atom_names = Array{String}(raw_xyz[2:end,1])
@@ -68,7 +76,9 @@ function xyz2matrix(input_xyz)
 end
 
 function xyz2internal(input_xyz)
-    # Imports .xyz file format to zmat, returns internal coord vector
+	"""
+	Imports .xyz file format to zmat, returns internal coord vector
+	"""
     babel_zmat = read(`obabel -ixyz $(input_xyz) -ogzmat`,String)
     regexmatch = match(r"Variables:",babel_zmat)
 	coord_segment_zmat = babel_zmat[regexmatch.offset:end]
