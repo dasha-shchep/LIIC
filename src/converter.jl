37843850f49fcd410f99cc96b3ac78cc229201d2
dasha-shchep <ds17094@bristol.ac.uk>
2022-03-01 18:02:59 +0000

    EOD

--- src/converter.jl
+++ src/converter.jl
@@ -5,6 +5,7 @@ Contains functions that convert between xyz and z-matrix formats to serve as
 input in the LIIC routines.
 
 """
+using LinearAlgebra
 
 export xyz_to_zmat, zmat_to_xyz
 
@@ -30,15 +31,21 @@ function import_molecule(xyz_file)
     return Molecule(atom_names,atom_coords,num_atom)
 end
 
-function bond(a1,a2)
-    return 0
+function bond(a1::Vector{Float64},a2::Vector{Float64})
+    return norm(a1-a2)
 end
 
-function angle(a1,a2,a3)
-    return 0
+function angle(a1::Vector{Float64},a2::Vector{Float64},a3::Vector{Float64})
+    bond1 = a1-a2
+    bond2 = a3-a2
+    angle = acosd(dot(bond1,bond2)/(norm(bond1)*norm(bond2)))
+    return angle
 end
 
 function dihedral(a1,a2,a3,a4)
+    bond1 = a2-a1
+    bond2 = a3-a2
+    bond3 = a4-a3
     return 0
 end
 
