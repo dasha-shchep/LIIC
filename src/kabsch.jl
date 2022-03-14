3b10cc415c7ffa81e41860c2d7f8d587c5fb8657
dasha-shchep <ds17094@bristol.ac.uk>
2022-03-14 17:56:23 +0000

    Generalizing kabsch

--- src/kabsch.jl
+++ src/kabsch.jl
@@ -10,16 +10,9 @@ export kabsch_rotate
 
 function translate_to_centroid(coord_matrix)
     # Normalises the molecular coordinates by centering them.
-    center = [mean(coord_matrix[:,1]);mean(coord_matrix[:,2]);mean(coord_matrix[:,3])]
-    centroid = transpose(center)
-    translated_geom = broadcast(-,coord_matrix,centroid)
+    center = mean(coord_matrix,dims=1)
+    translated_geom = coord_matrix .- center
     return translated_geom
-end
-
-function cross_covariance_matrix(Pmatrix,Qmatrix)
-    # Cross covariance matrix gives measure of variability between two matrices
-    CCmatrix = transpose(Pmatrix) * Qmatrix
-    return CCmatrix
 end
 
 function optimal_rotation_matrix(CCmatrix)
@@ -56,7 +49,8 @@ function kabsch_rotate(Pgeom,Qgeom)
     normalisedP = (translate_to_centroid(Pgeom))
     normalisedQ = (translate_to_centroid(Qgeom))
     
-    xcov = cross_covariance_matrix(normalisedP,normalisedQ)
+    # Calculate cross covariance matrix
+    xcov = transpose(normalisedP)*normalisedQ
     orot = optimal_rotation_matrix(xcov)
     
     num_atoms = size(normalisedP)[1]
