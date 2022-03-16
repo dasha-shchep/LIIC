cff95afd3589b66dec3d323b88e9b22a6cde948d
dasha-shchep <ds17094@bristol.ac.uk>
2022-03-16 17:28:16 +0000

    EOD

--- src/interpol.jl
+++ src/interpol.jl
@@ -21,40 +21,53 @@ function distance()
     println("execute dii")
 end
 
-function internal(int_arr_1,int_arr_2,steps,header)
-	"""
-	Takes array of Z-matrix internal coordinates and interpolates between them
-	to return an array of xyzs.
-	"""
-	numInternalCrd = size(int_arr_1)[1]
-	diffVec = Vector(undef,numInternalCrd)
-	for i in 1:numInternalCrd
-		if ( -180. <= (int_arr_2[i,2]-int_arr_1[i,2]) <= 180. )
-			diffVec[i] = (int_arr_2[i,2]-int_arr_1[i,2])/steps
-		elseif (int_arr_2[i,2] - int_arr_1[i,2]) < -180.
-			diffVec[i] = (int_arr_2[i,2] - int_arr_1[i,2] + 360.)/steps
-		elseif (int_arr_2[i,2]-int_arr_1[i,2]) > 180.
-			diffVec[i] = (int_arr_2[i,2] - int_arr_1[i,2] - 360.)/steps
-		else
-			println("Something has gone terribly wrong")
-		end
+function internal(zmat_start,zmat_end,steps)
+	int_start = zmat_start.IntVars
+	int_end = zmat_end.IntVars
+	natoms = zmat_end.Number
+	difference = (int_end - int_start)/(steps-1)
+	liic_points = Array{Float64,3}(undef,natoms,3,steps)
+	for step in 1:steps
+		liic_points[:,:,step] = int_start + step*difference
 	end
-	zmatArray = Vector(undef,steps)
-	interXYZ = Array{Float32,3}(undef,19,3,steps)
-	atom_names = Array{String,1}(undef,19)
-	for j in 1:steps
-		zmatArray[j] = header * "Variables:\n" * intlVec(int_arr_1,diffVec,j)
-		open("tmp","w") do io
-			write(io,zmatArray[j])
-		end
-		convXyz = read(`obabel -igzmat tmp -oxyz`,String)
-		run(`rm tmp`)
-		just_coords = Array{Float32}(readdlm(IOBuffer(convXyz),skipstart=2)[:,2:4])
-		atom_names = Array{String}(readdlm(IOBuffer(convXyz),skipstart=2)[:,1])
-		interXYZ[:,:,j] = just_coords
-	end
-	return interXYZ, atom_names
 end
+
+# OLD IMPLEMENTATION THAT USED OPEN BABEL
+# function internal(int_arr_1,int_arr_2,steps,header)
+# 	"""
+# 	Takes array of Z-matrix internal coordinates and interpolates between them
+# 	to return an array of xyzs.
+# 	"""
+	# numInternalCrd = size(int_arr_1)[1]
+	# diffVec = Vector(undef,numInternalCrd)
+	# for i in 1:numInternalCrd
+	# 	if ( -180. <= (int_arr_2[i,2]-int_arr_1[i,2]) <= 180. )
+	# 		diffVec[i] = (int_arr_2[i,2]-int_arr_1[i,2])/steps
+	# 	elseif (int_arr_2[i,2] - int_arr_1[i,2]) < -180.
+	# 		diffVec[i] = (int_arr_2[i,2] - int_arr_1[i,2] + 360.)/steps
+	# 	elseif (int_arr_2[i,2]-int_arr_1[i,2]) > 180.
+	# 		diffVec[i] = (int_arr_2[i,2] - int_arr_1[i,2] - 360.)/steps
+	# 	else
+	# 		println("Something has gone terribly wrong")
+	# 	end
+	# end
+	# zmatArray = Vector(undef,steps)
+	# interXYZ = Array{Float32,3}(undef,19,3,steps)
+	# atom_names = Array{String,1}(undef,19)
+	# for j in 1:steps
+	# 	zmatArray[j] = header * "Variables:\n" * intlVec(int_arr_1,diffVec,j)
+	# 	open("tmp","w") do io
+	# 		write(io,zmatArray[j])
+	# 	end
+	# 	convXyz = read(`obabel -igzmat tmp -oxyz`,String)
+	# 	run(`rm tmp`)
+	# 	just_coords = Array{Float32}(readdlm(IOBuffer(convXyz),skipstart=2)[:,2:4])
+	# 	atom_names = Array{String}(readdlm(IOBuffer(convXyz),skipstart=2)[:,1])
+	# 	interXYZ[:,:,j] = just_coords
+	# end
+	# return interXYZ, atom_names
+# end
+	# END OF OLD IMPLEMENTATION
 
 function intlVec(int_arr_1,diffVec,step)
 	"""
